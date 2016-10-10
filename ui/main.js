2b484f7a6c81d5e8b21fb4664250218db0f9f0a4
azeezgnh <abdulazeezkhan@bsauniv.ac.in>
2016-10-10 00:48:21 -0700

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,6 +4,12 @@ var element = document.getElementById('main-text')
 element.innerHTML = "New value";
 
 var img = document.getElementById('madi');
+function moveRight () {
+    marginLeft = marginLeft + 10 ;
+    img.style.marginLeft = marginLeft + 'px';
+    
+}
 img.onclick = function () {
-    img.style.marginLeft = '100px';
+    var interval = setInterval(moveRight, 100);
+    
 };
